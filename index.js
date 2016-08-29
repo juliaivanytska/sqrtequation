746ff4f873fbc4ce707e23ce22387b3536b37fa8
Yuliya <juliaivanytska15@gmail.com>
2016-08-29 22:13:34 +0000

    js

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-var  a,b,c,x,D;
-   D=b*b-4*a*c;
-   a=number;
-   b=number;
-   c=number;
-   x=number;
+var a = prompt('Введіть значення для a',100000);
+var b=prompt ('Введіть значення для b',100000);
+var c=promt ('Введіть значення для c',100000);
+var D=number;
 //sqrt 
-input ('a','b','c')
-document.write(Math.sqrt(D));
-if (true) {D=0 then
- x
-    
+function discriminant () {document.write('D'); D='b*b+4*a*c';}
+discriminant();
+function kvadrat(square root) {
 }
+if {
+    
+} (D=0) {
+   x=(-b)/2*a
+}
+if{
+if(D<0) {
+    x="НЕМА"
+}
+else{
+if (D>0) {x=((-b)+sqrt(D))/(2*a)} {x=((-b)+sqrt(D))/(2*a)};}
+   document.write('квадратний корінь дискримінанта' +Math.sqrt(D)+'<br>');
+   document.write(<br/>)
+  document.write('x' +x+ '<br>');
+  document.write(<br/>)
