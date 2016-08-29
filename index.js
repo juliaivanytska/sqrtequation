e47d3e967ed02280537048a00a0c1e040579b14a
Yuliya <juliaivanytska15@gmail.com>
2016-08-29 13:04:11 +0000

    sqrt

---
+++ index.js
@@ -0,0 +1,13 @@
+var  a,b,c,x,D;
+   D=b*b-4*a*c;
+   a=number;
+   b=number;
+   c=number;
+   x=number;
+//sqrt 
+input ('a','b','c')
+document.write(Math.sqrt(D));
+if (true) {D=0 then
+ x
+    
+}
